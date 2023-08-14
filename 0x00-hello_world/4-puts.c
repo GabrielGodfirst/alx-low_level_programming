219c31f605459cda216bfe8faf84424f48ad55d9
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2023-08-14 15:31:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+
 /**
- * This program prints "Programming is like building a multilingual puzzle, follow by a newline
- * Return: Always 0"
+ * main-print Programming is like building a multilingual puzzle,using the function
+ * puts instead of printf, followed by a  newline
  */
 
 int main(void)
 
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual pluzzle");
 
-	return(0);
+	return (0);
+
 }
