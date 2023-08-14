d85d026a2876d4ed326e214f1817c6b8a01a24e4
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2023-08-13 21:54:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This program prints :Programming is like building a multilingual puzzle, follow by aa newline
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzy");
+
+	return (0);
+}
