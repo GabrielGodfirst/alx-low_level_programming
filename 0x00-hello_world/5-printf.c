6ce3d8d15a68d43a40f9e3541e691a5c2de61705
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2023-08-14 17:39:17 -0700

    6-size

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success}
+ *
+ */
+
+int min(void)
+
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+
+        return (0);
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main-prints the size of various types based
+ * on the computer is compile and run on
+ * Return: Aleways 0
+ */
+
+int main(void)
+
+
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(int));
+
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long ing));
+
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
+
