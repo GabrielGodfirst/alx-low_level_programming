6346cfbc65919132fbdc42f3f1de120468315290
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2023-08-22 21:38:14 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcp - copy a string
+ * @dest: Destination value
+ * @src: Source value
+ * Return: Always return the pointer or dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
+}
