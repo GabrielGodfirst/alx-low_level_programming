3a9a4d441a9536465e36d165c9d12dd90b11be61
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2023-09-23 02:42:22 +0000

    6. Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	printf("(A tortoise, having pretty good sense of ahare's nature, "
-			"challenges one to a race.)\n");
+	printf("(A tortoise, having pretty good sense of ahare's nature,\n
+		challenges one to a race.)\n");
 	return (0);
 }
