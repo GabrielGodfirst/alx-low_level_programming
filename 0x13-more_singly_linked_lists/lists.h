a5c83386d9f4f94e19c060c73eafc212be6172d2
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2023-09-27 03:36:38 +0000

    0. Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @n: integer
@@ -8,11 +13,11 @@
  * Description: points to the next node
  */
 
-typedef struct list_s
+typedef struct listint_s
 {
 	int n;
-	struct list_s *next;
-} list_t;
+	struct listint_s *next;
+} listint_t;
 
 
 int _putchar(char c);
