3015c5fc2ffff3a458b658a45d67d734d3b101b4
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2023-10-06 03:35:05 +0000

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - prints txt file to stdio
+ * @filename: the txt fille
+ * @letters: txt numbers read
+ * Return: weight of file byte read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) *letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
