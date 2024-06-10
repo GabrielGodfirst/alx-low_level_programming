62da4fe731cf810ffc4478d0a62aeca45e7d0eee
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-06-10 10:10:00 +0000

    static libraries

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-0x09. C - Static libraries
+Readme for 0x09. C - Static libraries
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Calls the _puts function to print a quote by Kevin Mitnick
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+			"\t- Kevin Mitnick");
+
+	return (0);
+}
